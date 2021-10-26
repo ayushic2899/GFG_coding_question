9b0b488a2b176e4c5e667426c2b843bb30326b8e
Ayushi choudhary <67158285+ayushic2899@users.noreply.github.com>
2021-10-26 13:43:18 +0530

    Create Game of XOR .cpp

---
+++ PG-BitMagic/Game of XOR .cpp
@@ -0,0 +1,15 @@
+int gameOfXor(int N , int A[]) {
+        int result=0;
+        for(int i=0;i<N;i++){
+            if(i % 2 == 0){
+                result=result ^ A[i]; 
+            }
+        }
+        if(N%2==0){
+            return 0;
+        }
+        else{
+            return result;
+        }
+    }
+};
