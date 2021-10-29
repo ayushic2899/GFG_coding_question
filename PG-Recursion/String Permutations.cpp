b7bcf0678a8638d309a1465ec0f30c61b09bce2f
Ayushi choudhary <67158285+ayushic2899@users.noreply.github.com>
2021-10-29 21:18:09 +0530

    Update String Permutations.cpp

--- PG-Recursion/String Permutations.cpp
+++ PG-Recursion/String Permutations.cpp
@@ -1,1 +1,22 @@
-
+ void solve(string s, vector<string> &ans, int i)
+    {
+        if (i == s.size()-1)
+        {
+            ans.push_back(s);
+            return;
+        }
+            for (int j=i; j<s.size(); j++)
+        {
+            swap(s[i], s[j]);
+            solve(s, ans, i+1);
+            swap(s[i], s[j]);
+        }
+    }
+    
+    vector<string> permutation(string S)
+    {
+        vector<string> ans;
+        solve(S, ans, 0);
+        sort(ans.begin(), ans.end());
+        return ans;
+    }
